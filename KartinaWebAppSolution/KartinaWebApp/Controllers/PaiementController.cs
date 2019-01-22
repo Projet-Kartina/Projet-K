f9dd3c384cc5b12db78f6c64f1ee7f8bb799ae18
Ivana <ivanamarkova1@gmail.com>
2019-01-22 15:30:53 +0100

    fdfdf

---
+++ KartinaWebAppSolution/KartinaWebApp/Controllers/PaiementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KartinaWebApp.Controllers
+{
+    public class PaiementController : Controller
+    {
+        // GET: Paiement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
