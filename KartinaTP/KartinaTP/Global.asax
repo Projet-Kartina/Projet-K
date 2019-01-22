1da673911b8203d4b994941b9636394beabf87ee
Ivana <ivanamarkova1@gmail.com>
2019-01-22 09:41:28 +0100

    Archives projet Recap Evan MVC/Front

---
+++ KartinaTP/KartinaTP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KartinaTP.MvcApplication" Language="C#" %>
---
+++ KartinaTP/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Determine si dans l'onglet format, l'utilisateur a bien choisi une option
+        /// </summary>
+        [TestMethod]
+        public void TestSelectionOngletFormat()
+        {
+            ObjetATester objetATesterOngletFormat = new ObjetATester();
+            Assert.IsNotNull(objetATesterOngletFormat);
+        }
+        /// <summary>
+        /// Determine si dans l'onglet finition, l'utilisateur a bien choisi une option
+        /// </summary>
+        [TestMethod]
+        public void TestSelectionOngletFinition()
+        {
+            ObjetATester objetATesterOngletFinition = new ObjetATester();
+            Assert.IsNotNull(objetATesterOngletFinition);
+        }
+        /// <summary>
+        /// Determine si dans l'onglet cadre, l'utilisateur a bien choisi une option
+        /// </summary>
+        [TestMethod]
+        public void TestSelectionOngletCadre()
+        {
+            ObjetATester objetATesterOngletCadre = new ObjetATester();
+            Assert.IsNotNull(objetATesterOngletCadre);
+        }
+    }
+}
